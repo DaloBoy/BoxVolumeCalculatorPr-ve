b0a8016fbc5e53497fd8f2104efb1cc5f698d452
Christian Oftelund Jensen <chri9j83@edu.easj.dk>
2017-10-23 11:31:18 +0200

    har lavet simple tests

---
+++ BoxVolumeCalculatorPrøveTests/Service1Tests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BoxVolumeCalculatorPrøve;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxVolumeCalculatorPrøve.Tests
+{
+    [TestClass()]
+    public class Service1Tests
+    {
+        [TestMethod()]
+        public void GetDataTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetDataUsingDataContractTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSideTestSuccess()
+        {
+            //Arrange
+            var box = new Service1();
+            //Act
+            var test = box.GetSide(40, 2, 5);
+            //Assert
+            Assert.AreEqual(4, test);
+        }
+       
+        [TestMethod()]
+        public void GetVolumeTestSuccess()
+        {
+            //Arrange
+            var box = new Service1();
+            //Act
+            var test = box.GetVolume(4, 5, 2);
+            //Assert
+            Assert.AreEqual(40, test);
+        }
+    
+    }
+}
