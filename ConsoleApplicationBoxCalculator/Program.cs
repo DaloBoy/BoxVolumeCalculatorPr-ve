eafd7244318a3d7e41bcc8a0a39b1fc431f65976
Christian Oftelund Jensen <chri9j83@edu.easj.dk>
2017-10-23 11:39:43 +0200

    Har lavet en simpel console application der kan vise de udregninger der er i service1 bliver brugt

---
+++ ConsoleApplicationBoxCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BoxVolumeCalculatorPrøve;
+
+namespace ConsoleApplicationBoxCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Service1 service = new Service1();
+            Console.WriteLine("Volume: 1*2*5 ");
+            Console.WriteLine(service.GetVolume(1,2,5));
+            Console.WriteLine("/n");
+            Console.WriteLine("Missing side: 20/(2*2) ");
+            Console.WriteLine(service.GetSide(20, 2, 2));
+            Console.ReadLine();
+
+        }
+    }
+}
